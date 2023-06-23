5fbff0a499305105a5ea379be8b700c3475354b4
Andrei Stanciu <andrei.stanciu@idea-bank.ro>
2023-06-23 13:09:46 +0300

    Commit 3

--- Today/Models/DetailViewController/ReminderViewController.swift
+++ Today/Models/DetailViewController/ReminderViewController.swift
@@ -27,6 +27,12 @@ class ReminderViewController: UICollectionViewController {
             return collectionView.dequeueConfiguredReusableCell(
                 using: cellRegistration, for: indexPath, item: itemIdentifier)
         }
+        
+        if #available(iOS 16, *) {
+            navigationItem.style = .navigator
+        }
+        navigationItem.title = NSLocalizedString("Reminder", comment: "Reminder view controller title")
+        
         updateSnapshot()
     }
     
